11c7f7e2d427e55c4da10dd0d4415cc718c68537
Senen Urdaneta <senenurdaneta@gmail.com>
2023-08-21 20:01:59 -0300

    Final. Modifique la funcion y programa principal.

--- 3-Frecuencia.py
+++ 3-Frecuencia.py
@@ -1,24 +1,24 @@
 # Escribir un programa que reciba una cadena de caracteres y devuelva un diccionario con
 # cada palabra que contiene y la cantidad de veces que aparece (frecuencia).
-
-def contarpalabras(cadena):
+def cuentapalabras(cadena):
     palabras = cadena.split()
-    return palabras
+    dic = {}
+    # Recorremos la cadena 
+    for palabra in palabras:
+        palabra = palabra.strip(".,!¡¿?\"'()[]{}") # Quitamos los Caracteres especiales
+        palabra = palabra.lower() # Convertimos las palabras en minuscula
+        # Contamos las palabras repetidas  y las cargamos en el diccionario
+        if palabra in dic:
+            dic[palabra] = dic[palabra] + 1 
+        else:
+            dic[palabra] = 1
+    return dic
 
-dic = {}
-parrafo = input("Ingrese Parrafo:")
-resultado = contarpalabras(parrafo)
-# Recorremos la cadena Quitamos los Caracteres especiales y convertimos las palabras en minuscula
-for palabra in resultado:
-    palabra = palabra.strip(".,!¡¿?\"'()[]{}")
-    palabra = palabra.lower()
-    # Contamos las palabras repetidas  y las cargamos en el diccionario
-    if palabra in dic:
-        dic[palabra] = dic[palabra] + 1 
-    else:
-        dic[palabra] = 1
+# Programa Principal
+parrafo = input("Ingrese una Cadena o Parrafo:")
+resultado = cuentapalabras(parrafo)
 # Recorremos el diccionaro y lo imprimo
-for palabra, veces in dic.items():
+for palabra, veces in resultado.items():
     print(f"{palabra}: {veces}")
 
 
