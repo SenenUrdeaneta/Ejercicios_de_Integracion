210547ad237b34c834feeb40469f218b17fd03a9
Senen Urdaneta <senenurdaneta@gmail.com>
2023-08-21 21:00:17 -0300

    Cmabio de nombre de archivos

---
+++ tupla.py
@@ -0,0 +1,10 @@
+# Escribir una función que reciba una cadena de caracteres y devuelva un diccionario con cada
+# palabra que contiene y la cantidad de veces que aparece (frecuencia). Escribir otra función
+# que reciba el diccionario generado con la función anterior y devuelva una tupla con la
+# palabra más repetida y su frecuencia.
+
+from 3-frecuencia import cuentapalabras
+
+resultado = cuentapalabras("Esto es una prueba, de las prueba donde se repiten las palabras y palabras")
+
+print(resultado)
